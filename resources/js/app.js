945b3315c19f11636ede19b9b89f285fadd9b93f
Julian <juliansznaider@thenextweb.com>
2024-06-02 23:36:50 -0300

    post call to the actual route

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,9 @@ import './bootstrap';
 import Sortable from 'sortablejs/modular/sortable.complete.esm.js';
 
 let list = document.getElementById('list');
+const url = list.dataset.updateUrl;
+const token = document.querySelector('meta[name="csrf-token"]').attributes['content'].value
+
 let sort = new Sortable(list, {
     onSort: function (event) {
 		let tasks_list = list.querySelectorAll('li')
@@ -9,20 +12,20 @@ let sort = new Sortable(list, {
         tasks_list.forEach((task) => {
             tasks.push(task.dataset.id)
         })
-        
         const options = {
             method: 'POST',
             headers: {
             'Content-Type': 'application/json',
+            'X-CSRF-TOKEN': token
             },
-            body: JSON.stringify(update),
+            body: JSON.stringify({ 'tasks' : tasks }),
         };
 
         fetch(
-            'https://tasks.free.beeceptor.com',
-            []
+            url,
+            options
         ).then(response => {
-                console.log(response);
+                console.log(response.json());
         }).catch(err => console.error(err))
 
 	},
