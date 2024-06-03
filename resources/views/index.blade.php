2cea90f582dca2345c22fc67ace154e547d58206
Julian <juliansznaider@thenextweb.com>
2024-06-03 08:16:42 -0300

    add some styling

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,30 +14,33 @@
     </head>
     <body class="font-sans antialiased dark:bg-black dark:text-white/50">
         <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
-            <img id="background" class="absolute -left-20 top-0 max-w-[877px]" src="https://laravel.com/assets/img/welcome/background.svg" />
             <div class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
                 <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
-                    <header class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
+                    <header class="grid grid-cols-2 items-center gap-2 pt-5 lg:grid-cols-3">
                         <div class="flex lg:justify-center lg:col-start-2">
-                            <h1>Tasks</h1>
+                            <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Task Manager</h1>
                         </div>
                     </header>
 
                     <main class="mt-12">
                         <div class="grid gap-12">
-                            <select 
-                                name="project_id" 
-                                id="project_id"
-                                class="">
-                                @foreach($projects as $project)
-                                    <option value="{{ $project->id }}">{{ $project->name }}</option>
-                                @endforeach
-                            </select>
-                            <ul id="list" data-update-url={{ route('UpdateTasksPriority') }}>
+                            <div>
+                                <label for="project_id" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Filter tasks by project</label>
+                                <select 
+                                    name="project_id" 
+                                    id="project_id"
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                    @foreach($projects as $project)
+                                        <option value="{{ $project->id }}">{{ $project->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <ul id="list" data-update-url={{ route('UpdateTasksPriority') }} class="max-w-md divide-y divide-gray-200 dark:divide-gray-70">
                                 @foreach($tasks as $task)
                                     <li 
                                         data-id="{{ $task->id }}" 
                                         data-project-id="{{ $task->project_id }}"
+                                        class="pl-3 pb-3 pt-3 cursor-pointer"
                                     >{{ $task->name }}</li>
                                 @endforeach
                             </ul>
@@ -45,7 +48,7 @@
                     </main>
 
                     <footer class="py-16 text-center text-sm text-black dark:text-white/70">
-                        Made by Julian Sznaider for Coalition Technologies
+                        Made by <a href="https://juliandev.vercel.app/" target="_blank" class="underline text-blue-600 hover:text-blue-800 visited:text-blue-600">Julian Sznaider</a> for Coalition Technologies
                     </footer>
                 </div>
             </div>
