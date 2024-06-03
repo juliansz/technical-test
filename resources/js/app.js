45549a49285d5770a54fe881ba986421dce91280
Julian <juliansznaider@thenextweb.com>
2024-06-02 22:49:07 -0300

    declar the onSort event to be able to update the priorities on the backend

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,13 @@
 import './bootstrap';
 import Sortable from 'sortablejs/modular/sortable.complete.esm.js';
 
-var list = document.getElementById('list');
-var sort = Sortable.create(list);
+let list = document.getElementById('list');
+let sort = new Sortable(list, {
+    onSort: function (event) {
+		let tasks = list.querySelectorAll('li')
+        tasks.forEach((task) => {
+            console.log(task)
+        })
+
+	},
+})
